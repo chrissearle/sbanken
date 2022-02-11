3a2e55719dbabbc9533bc9bc736bda8889a5ace4
Chris Searle <chris@chrissearle.org>
2022-02-11 13:57:55 +0100

    Get tooltips working

--- vue/src/components/AccountDetail.vue
+++ vue/src/components/AccountDetail.vue
@@ -7,14 +7,14 @@
       <div class="card col">
         <div class="card-body">
           <div class="card-text">
-            <bar-chart :chartData="chartData" />
+            <bar-chart v-bind="barChartProps" />
           </div>
         </div>
       </div>
       <div class="card col">
         <div class="card-body">
           <div class="card-text">
-            <line-chart :chartData="balanceChartData" />
+            <line-chart v-bind="balanceChartProps" />
           </div>
         </div>
       </div>
@@ -37,102 +37,122 @@
 </template>
 
 <script lang="ts">
-  import { defineComponent } from 'vue'
-  import ApiService from '@/services/ApiService'
-  import ResponseData from '@/types/ResponseData'
-  import TransactionList from '@/types/TransactionsList'
-  import TransactionSum from '@/types/TransactionsSum'
-  import {
-    amount,
-    sumTransactionsByDay,
-    balanceTransactionsByDay,
-  } from '@/utils'
-  import moment from 'moment'
-  import Account from '@/types/Account'
-  import AccountOverview from './AccountOverview.vue'
-  import { BarChart, LineChart } from 'vue-chart-3'
-  import { Chart, registerables } from 'chart.js'
+import { defineComponent } from "vue";
+import ApiService from "@/services/ApiService";
+import ResponseData from "@/types/ResponseData";
+import TransactionList from "@/types/TransactionsList";
+import TransactionSum from "@/types/TransactionSum";
+import { amount, sumTransactionsByDay, balanceTransactionsByDay } from "@/utils";
+import moment from "moment";
+import Account from "@/types/Account";
+import AccountOverview from "./AccountOverview.vue";
+import { BarChart, LineChart } from "vue-chart-3";
+import { Chart, TooltipItem, registerables } from "chart.js";
 
-  Chart.register(...registerables)
+Chart.register(...registerables);
 
-  export default defineComponent({
-    components: { AccountOverview, BarChart, LineChart },
-    name: 'account-detail',
-    data() {
-      return {
-        transactions: {} as TransactionList | null,
-        account: {} as Account | null,
+export default defineComponent({
+  components: { AccountOverview, BarChart, LineChart },
+  name: "account-detail",
+  data() {
+    return {
+      transactions: {} as TransactionList | null,
+      account: {} as Account | null,
+      barChartProps: {
         chartData: {
-          labels: [''],
+          labels: [""],
           datasets: [
             {
-              label: 'Amount by day',
+              label: "Amount by day",
               data: [0],
             },
           ],
         },
-        balanceChartData: {
-          labels: [''],
+        options: {
+          plugins: {
+            tooltip: {
+              callbacks: {
+                label: function (item: TooltipItem<"bar">) {
+                  return amount(item.parsed.y);
+                },
+              },
+            },
+          },
+        },
+      },
+      balanceChartProps: {
+        chartData: {
+          labels: [""],
           datasets: [
             {
-              label: 'Balance',
+              label: "Balance",
               data: [0],
             },
           ],
         },
-      }
-    },
-    props: {
-      id: String,
-    },
-    methods: {
-      displayAmount(val: number): string {
-        return amount(val)
+        options: {
+          plugins: {
+            tooltip: {
+              callbacks: {
+                label: function (item: TooltipItem<"bar">) {
+                  return amount(item.parsed.y);
+                },
+              },
+            },
+          },
+        },
       },
-      displayDate(val: string): string {
-        return moment(val).format('DD-MM-YYYY')
-      },
+    };
+  },
+  props: {
+    id: String,
+  },
+  methods: {
+    displayAmount(val: number): string {
+      return amount(val);
     },
-    mounted() {
-      if (this.id) {
-        ApiService.getAccount(this.id)
-          .then((response: ResponseData) => {
-            this.account = response.data
-
-            ApiService.getTransactions(this.id)
-              .then((response: ResponseData) => {
-                this.transactions = response.data
-
-                if (this.transactions?.items) {
-                  const transactionSum = sumTransactionsByDay(
-                    this.transactions.items
-                  )
-
-                  const labels = transactionSum.map((tx) => tx.accountingDate)
-                  const sumAmounts = transactionSum.map((tx) => tx.amount)
-
-                  labels.reverse()
-                  sumAmounts.reverse()
-
-                  const balanceSum = balanceTransactionsByDay(
-                    sumAmounts,
-                    this.account?.balance || 0
-                  )
-
-                  this.$data.chartData.labels = labels
-                  this.$data.chartData.datasets[0].data = sumAmounts
-                  this.$data.balanceChartData.labels = labels
-                  this.$data.balanceChartData.datasets[0].data = balanceSum
-                }
-              })
-              .catch((e: Error) => {
-                console.log(e)
-              })
-          })
-          .catch((e: Error) => {
-            console.log(e)
-          })
-      }
+    displayDate(val: string): string {
+      return moment(val).format("DD-MM-YYYY");
     },
-  })
+  },
+  mounted() {
+    if (this.id) {
+      ApiService.getAccount(this.id)
+        .then((response: ResponseData) => {
+          this.account = response.data;
+
+          ApiService.getTransactions(this.id)
+            .then((response: ResponseData) => {
+              this.transactions = response.data;
+
+              if (this.transactions?.items) {
+                const transactionSum = sumTransactionsByDay(this.transactions.items);
+
+                const labels = transactionSum.map((tx) => tx.accountingDate);
+                const sumAmounts = transactionSum.map((tx) => tx.amount);
+
+                labels.reverse();
+                sumAmounts.reverse();
+
+                const balanceSum = balanceTransactionsByDay(
+                  sumAmounts,
+                  this.account?.balance || 0
+                );
+
+                this.$data.barChartProps.chartData.labels = labels;
+                this.$data.barChartProps.chartData.datasets[0].data = sumAmounts;
+                this.$data.balanceChartProps.chartData.labels = labels;
+                this.$data.balanceChartProps.chartData.datasets[0].data = balanceSum;
+              }
+            })
+            .catch((e: Error) => {
+              console.log(e);
+            });
+        })
+        .catch((e: Error) => {
+          console.log(e);
+        });
+    }
+  },
+});
 </script>
